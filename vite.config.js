fbe36f4c481f25ac9bd7d5a33e0a6fa4d70505e7
Aditya sai chenjeri <adityasai0330@gmail.com>
2025-02-14 14:11:27 +0530

    Adding codes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+//import { defineConfig } from 'vite'
+//import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+//export default defineConfig({
+//  plugins: [react()],
+//})
+
+/// vitest.config.js
+import { defineConfig } from 'vitest/config';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  test: {
+    globals: true,  // Allows using `expect` globally
+    environment: 'jsdom', // Required for testing React components
+    setupFiles: './setupTests.js', // Loads setup file
+  },
+});
